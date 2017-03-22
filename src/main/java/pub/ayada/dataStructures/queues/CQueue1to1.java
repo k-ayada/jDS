7df5d84efaecd96a0142ccb43034c835ca5802a4
k-ayada <kiran14n@gmail.com>
2017-03-22 17:43:05 +0530

    Update CQueue1to1.java

--- src/main/java/pub/ayada/dataStructures/queues/CQueue1to1.java
+++ src/main/java/pub/ayada/dataStructures/queues/CQueue1to1.java
@@ -2,16 +2,16 @@ package pub.ayada.dataStructures.queues;
 
 public class CQueue1to1<E> extends CQueue<E> {
 	private static final long serialVersionUID = -3952115187196292811L;
-	public CQueue1to1(int CQueue_Length) {
-		super(CQueue_Length);
+	public CQueue1to1(String queueID, int CQueue_Length) {
+		super(queueID,CQueue_Length);
 	}
-	public CQueue1to1(int CQueue_Length, boolean testing ) {
-		super(CQueue_Length, testing);
+	public CQueue1to1(String queueID,int CQueue_Length, boolean testing ) {
+		super(queueID,CQueue_Length, testing);
 	}
-	public CQueue1to1(int CQueue_Length, CQueue<E> clone) {
-		super(CQueue_Length, clone);
+	public CQueue1to1(String queueID,int CQueue_Length, CQueue<E> clone) {
+		super(queueID,CQueue_Length, clone);
 	}	
-	public CQueue1to1(CQueue<E> clone) {
-		super(clone);
+	public CQueue1to1(String newQueueID,CQueue<E> clone) {
+		super(newQueueID,clone);
 	}	
 }
